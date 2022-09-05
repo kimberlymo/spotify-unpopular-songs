48f6f54021f7cebc80ce6e0c398bcc9956de1dc4
kimberlymo <kimberly.moorhouse@lernende.bbw.ch>
2022-09-05 10:23:54 +0200

    entities added

---
+++ src/main/java/ch/bbw/modul151/spotifyunpopularsongs/Artist.java
@@ -0,0 +1,7 @@
+package ch.bbw.modul151.spotifyunpopularsongs;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Artist {
+}
--- src/main/java/ch/bbw/modul151/spotifyunpopularsongs/SpotifyUnpopularSongsApplication.java
+++ src/main/java/ch/bbw/modul151/spotifyunpopularsongs/SpotifyUnpopularSongsApplication.java
@@ -1,13 +1,22 @@
 package ch.bbw.modul151.spotifyunpopularsongs;
 
 import org.springframework.boot.SpringApplication;
+import org.h2.tools.Server;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class SpotifyUnpopularSongsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpotifyUnpopularSongsApplication.class, args);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
     }
 
 }
