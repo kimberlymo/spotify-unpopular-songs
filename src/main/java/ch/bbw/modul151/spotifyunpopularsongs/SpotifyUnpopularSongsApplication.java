b6f2cc9ea075d871be87f316b62876aba418803a
kimberlymo <kimberly.moorhouse@lernende.bbw.ch>
2022-09-05 09:12:05 +0200

    first commit

---
+++ src/main/java/ch/bbw/modul151/spotifyunpopularsongs/SpotifyUnpopularSongsApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.modul151.spotifyunpopularsongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyUnpopularSongsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyUnpopularSongsApplication.class, args);
+    }
+
+}
