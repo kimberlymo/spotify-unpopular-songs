fdd899aa5555e6262d74d4a815fb70217bd6af47
kimberlymo <kimberly.moorhouse@lernende.bbw.ch>
2022-09-12 15:15:21 +0200

    updated dataset (not completed!)

--- src/main/java/ch/bbw/modul151/spotifyunpopularsongs/track/TrackRepository.java
+++ src/main/java/ch/bbw/modul151/spotifyunpopularsongs/track/TrackRepository.java
@@ -3,8 +3,12 @@ package ch.bbw.modul151.spotifyunpopularsongs.track;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TrackRepository extends JpaRepository<Track, String> {
 
     void deleteTrackByTrackArtistIs(String artistToDelete);
+
+    List<Track> findByTrackArtist(String artistName);
 }
--- src/test/java/ch/bbw/modul151/spotifyunpopularsongs/track/TrackRepositoryTest.java
+++ src/test/java/ch/bbw/modul151/spotifyunpopularsongs/track/TrackRepositoryTest.java
@@ -1,10 +1,29 @@
 package ch.bbw.modul151.spotifyunpopularsongs.track;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TrackRepositoryTest {
+
+    private TrackRepository repository;
+    private String artistNameToDelete;
+
+    @Test
+    void deleteIsSuccessful() {
+        artistNameToDelete = "James Reeder";
+
+        assertEquals(1000, repository.count());
+        repository.deleteTrackByTrackArtistIs(artistNameToDelete);
+        assertEquals(975, repository.count());
+        assertEquals(0, repository.findByTrackArtist(artistNameToDelete).size());
+    }
+
+    @Test
+    void artistNotFound() {
+        artistNameToDelete = "bobby123455";
+    }
 
 }
