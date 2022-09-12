4c558551d28cb037548dae99b8685e53b6c14fb3
kimberlymo <kimberly.moorhouse@lernende.bbw.ch>
2022-09-12 08:38:31 +0200

    entities added

--- src/main/java/ch/bbw/modul151/spotifyunpopularsongs/Artist.java
+++
@@ -1,7 +0,0 @@
-package ch.bbw.modul151.spotifyunpopularsongs;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Artist {
-}
---
+++ src/main/java/ch/bbw/modul151/spotifyunpopularsongs/Track.java
@@ -0,0 +1,30 @@
+package ch.bbw.modul151.spotifyunpopularsongs;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Track {
+
+    @Id
+    private String trackId;
+    private Double danceability;
+    private Double energy;
+    private Double trackKey;
+    private Double loudness;
+    private Double mode;
+    private Double speechiness;
+    private Double acousticness;
+    private Double instrumentalness;
+    private Double liveness;
+    //private Double valence;
+    //private Double tempo;
+    //private Double durationMs;
+    //private Boolean explicit;
+    //private Double popularity;
+    //private String trackName;
+    ////forgein key
+    //private String trackArtist;
+    //
+//
+}//
