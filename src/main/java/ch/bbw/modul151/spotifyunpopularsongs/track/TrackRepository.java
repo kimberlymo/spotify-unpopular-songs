e04363d79e0de7272b5cb3b545cd9ffadfc5d009
nils <48135649+Nilstrieb@users.noreply.github.com>
2022-09-26 08:41:32 +0200

    fix import

--- src/main/java/ch/bbw/modul151/spotifyunpopularsongs/track/TrackRepository.java
+++ src/main/java/ch/bbw/modul151/spotifyunpopularsongs/track/TrackRepository.java
@@ -1,5 +1,6 @@
 package ch.bbw.modul151.spotifyunpopularsongs.track;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
